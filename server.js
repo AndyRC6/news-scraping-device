f5bb19e2223bfd56c1466fdd609b4e3a76a9fd31
Andrew Reyes <Andyrc6@gmail.com>
2018-02-04 19:44:25 -0700

    modified secure cookies

--- server.js
+++ server.js
@@ -16,14 +16,17 @@ app.set("view engine", "handlebars");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, '/public')));
+app.set('trust proxy', 1)
 app.use(session({
     secret: 'dont die',
     resave: true,
     saveUninitialized: false,
     store: new MongoStore({
         mongooseConnection: mongoose.createConnection('mongodb://heroku_0vg7r2nr:fdqb2sr9c9vc9bd4uc92afrqg@ds123728.mlab.com:23728/heroku_0vg7r2nr')
-      })
+      }),
+      cookie: {secure: true}
   }));
+  
 
   
   mongoose.Promise = Promise;
