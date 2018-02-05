5d37119a734fba137fa6b7f8ff2c8c99049e1e79
Andrew Reyes <Andyrc6@gmail.com>
2018-02-04 19:25:53 -0700

    added mongo session storage

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ var bodyParser = require("body-parser");
 var exphbs = require("express-handlebars");
 var path = require('path');
 var session = require("express-session");
+var mongoose = require("mongoose");
+var MongoStore = require("connect-mongo")(session);
 
 var app = express();
 var port = process.env.PORT || 3000;
@@ -17,7 +19,10 @@ app.use(express.static(path.join(__dirname, '/public')));
 app.use(session({
     secret: 'dont die',
     resave: true,
-    saveUninitialized: false
+    saveUninitialized: false,
+    store: new MongoStore({
+        mongooseConnection: mongoose.createConnection('mongodb://heroku_0vg7r2nr:fdqb2sr9c9vc9bd4uc92afrqg@ds123728.mlab.com:23728/heroku_0vg7r2nr')
+      })
   }));
 //=======Routes=======
 require("./routes/pageroutes")(app);
